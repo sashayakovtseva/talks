926932b4992ce9ead9b8bf9b101c23b0f1d1f838
sashayakovtseva <sashayakovtseva@gmail.com>
2019-09-10 19:34:01 +0300

    Add dummy benchmarks

---
+++ net/listen_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net"
+	"testing"
+)
+
+func BenchmarkListenAll(b *testing.B) {
+	addr := ":8080"
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b.StartTimer()
+		ln, err := net.Listen("tcp", addr)
+		b.StopTimer()
+		if err != nil {
+			b.Fatalf("Listen error: %v", err)
+		}
+		ln.Close()
+	}
+}
+
+func BenchmarkListenSingle(b *testing.B) {
+	addr := "127.0.0.1:8080"
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b.StartTimer()
+		ln, err := net.Listen("tcp", addr)
+		b.StopTimer()
+		if err != nil {
+			b.Fatalf("Listen error: %v", err)
+		}
+		ln.Close()
+	}
+}

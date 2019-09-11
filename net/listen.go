f20aa125231362fd5abc0e2d3e252aa56e14ee55
sashayakovtseva <sashayakovtseva@gmail.com>
2019-09-11 17:00:21 +0300

    Update tests

--- net/listen.go
+++ net/listen.go
@@ -6,18 +6,21 @@ import (
 	"net"
 )
 
-var addr string
+var (
+	addr string
+	netw string
+)
 
 func init() {
 	flag.StringVar(&addr, "addr", ":8080", "server address to listen")
+	flag.StringVar(&netw, "net", "tcp", "network to use")
 }
 
 func main() {
 	flag.Parse()
 
 	log.Printf("Listening on %s", addr)
-	// ln, err := net.Listen("tcp6", addr)
-	ln, err := net.Listen("tcp", addr)
+	ln, err := net.Listen(netw, addr)
 	if err != nil {
 		log.Fatalf("Could not listen: %v", err)
 	}
--- net/listen_test.go
+++ net/listen_test.go
@@ -6,29 +6,51 @@ import (
 )
 
 func BenchmarkListenSingle(b *testing.B) {
-	addr := "127.0.0.1:8080"
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		b.StartTimer()
-		ln, err := net.Listen("tcp", addr)
-		b.StopTimer()
-		if err != nil {
-			b.Fatalf("Listen error: %v", err)
-		}
-		ln.Close()
+	tt := []struct {
+		netw string
+		addr string
+	}{
+		{
+			netw: "tcp",
+			addr: "127.0.0.1:8080",
+		},
+		{
+			netw: "tcp4",
+			addr: "127.0.0.1:8080",
+		},
+		{
+			netw: "tcp6",
+			addr: "[::1]:8080",
+		},
+	}
+	for _, tc := range tt {
+		b.Run(tc.netw, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				ln, err := net.Listen(tc.netw, tc.addr)
+				if err != nil {
+					b.Fatalf("Listen error: %v", err)
+				}
+				ln.Close()
+			}
+		})
 	}
 }
 
 func BenchmarkListenAll(b *testing.B) {
-	addr := ":8080"
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		b.StartTimer()
-		ln, err := net.Listen("tcp", addr)
-		b.StopTimer()
-		if err != nil {
-			b.Fatalf("Listen error: %v", err)
-		}
-		ln.Close()
+	tt := []string{
+		"tcp",
+		"tcp4",
+		"tcp6",
+	}
+	for _, tc := range tt {
+		b.Run(tc, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				ln, err := net.Listen(tc, ":8080")
+				if err != nil {
+					b.Fatalf("Listen error: %v", err)
+				}
+				ln.Close()
+			}
+		})
 	}
 }
